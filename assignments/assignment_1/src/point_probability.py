10f51584a36b2fadf04bc662297ea701785b67d4
Ivar Haugaløkken Stangeby <istangeby@gmail.com>
2015-03-09 13:56:22 +0100

    Nearly done, need to merge with compphys comp

--- assignments/assignment_1/src/point_probability.py
+++ assignments/assignment_1/src/point_probability.py
@@ -23,11 +23,19 @@ def S(x):
 def p(x):
   return (1 - S(x)) - (1-S(x-1))
 
+def E_hx():
+  return 1000000*sum([p(x)/(1.03**x) for x in range(0, 34)])
+
+def E_gx():
+  return (sum([p(x) for x in range(0, 77)]) - sum([(1/1.03)**(x+1)*p(x) for x in range(0, 34)])- sum([(1/1.03)**(34+1)*p(x) for x in range(34, 77)]))/(1 - 1/1.03)
+
 point_prob = []
 for x in range(76+1):
   point_prob.append(p(x))
 
-plt.axes([0, 100, 0, 0.05])
-plt.plot(range(0, 77), point_prob)
+plt.plot(range(0, 77), point_prob, 'ro')
 plt.legend(["$p(x), \quad x = 0, 1, \ldots, 76$"])
-plt.show()
+plt.savefig("point_probability.pdf", format='pdf')
+
+print "Expected value of payment of compensation E(h(X)): ", E_hx()
+print "Expected number of annual payments E(g(X)):        ", E_gx()
